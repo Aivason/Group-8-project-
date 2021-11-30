93b719bc1240934b5334d6588652b58bef20dd45
Phyhk <94524417+Phyhk@users.noreply.github.com>
2021-11-30 14:29:14 +0000

    example programme for titration curve

--- G8 project-1.py
+++ G8 project-1.py
@@ -5,3 +5,33 @@ PUT YOUR NAME HERE.
 """
 
 ##This is a programme for pH calculations and its titration plot.
+import pylab
+
+
+---------------------------------
+
+##example of the programme of the titration curve
+##titration of 100ml of 2M hcl with 1M NaOH
+
+a1,b1,a2=2,0.1,1
+c,b2=a1*b1,a1*b1/a2
+
+n,eps=50,1.e-10
+db = b2/n
+
+bs1=pylab.linspace(db,b2,n)
+bs2=pylab.linspace(b2,2*b2,n)
+
+as1=-pylab.log10((c-a2*bs1+eps)/(b1+bs2))
+as2=14+pylab.log10((a2*bs2-c+eps)/(b1+bs2))
+
+pylab.plot(bs1,as1,color='r')
+pylab.plot([bs1[-1],bs2[1]],[as1[-1],as2[1]],color='r')
+pylab.plot(bs2[1:],as2[1:],color='r')
+
+pylab.title('titration curve')
+pylab.xlabel('vol of naoh added')
+pylab.ylabel('pH')
+pylab.grid()
+
+pylab.show()
