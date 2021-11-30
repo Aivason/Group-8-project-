a95fe4a0495449ba783355e2d4f2e1e913f63218
Phyhk <94524417+Phyhk@users.noreply.github.com>
2021-11-30 13:55:42 +0000

    Update G8 project-1.py

--- G8 project-1.py
+++ G8 project-1.py
@@ -1,13 +1,7 @@
-
-# -*- coding: utf-8 -*-
 """
 Group 8 project Assignment
-
-This is a temporary script file.
+Contributors:
+PUT YOUR NAME HERE.
 """
 
-##Programme starts in next line.
-
 ##This is a programme for pH calculations and its titration plot.
-
-##lets start
