c6acdc23fd5134f601705ca2d0929a9ce5246451
poppygordon <94385556+poppygordon@users.noreply.github.com>
2021-12-11 13:42:57 +0000

    removing the acid base dictionary-no longer needed

--- G8 project-1.py
+++ G8 project-1.py
@@ -6,48 +6,6 @@ PUT YOUR NAME HERE.
 
 ##This is a programme for pH calculations and its titration plot.
 
-weakacids = {'Oxalic Acid': 1.27,'Sulfurous Acid': 1.85,'Phosphoric Acid':2.16,
-             'Nitrous Acid': 3.25,'Hydrofluoric Acid':3.20,'Methanoic Acid':3.75,
-             'Benzoic Acid':4.204 ,'Acetic Acid':4.75,'Formic Acid':3.75
-             }
-#dictionary of weakacids and their pka values
-
-weakacidslist = list(weakacids.keys())
-for c in range(len(weakacidslist)):
-    print(c+1,':',weakacidslist[c])
-
-#prints a list of all weakacids for user to choose from
-
-strongacids = {'Hydrochloric Acid':'-6.3','Nitric Acid':'-1.5','Sulfuric Acid':'-3', 
-               'Hydrobromic Acid': '-8.7','Hydroiodic Acid':'-9.3',
-               'Perchloric Acid':'-8','Chloric Acid': '1.0'
-                  }
-#dict of strong acids and their pka values
-#sulfuric acid is first dissociation
-
-strongacidslist = list(strongacids.keys())
-for c in range(len(strongacidslist)):
-    print(c+10,':',strongacidslist[c])
-#prints a list of all strong acids for user to choose from
-
-acidnum = int(input('What acid would you like to use? (enter the number beside the acid): '))
-try:
-    if acidnum>10:
-        acidindex = (acidnum-10)
-        acid = strongacidslist[acidindex]
-        print(acid)
-    else:
-        acid = weakacidslist[acidnum-1]
-        print(acid)
-except:
-    print('Error! Please enter a number between 1 and 16!')
-
-#asks user for input number
-#tells user to trpe another number if its out of range
-#if in range, prints acid. Able to use acid variable for calculations and recieve the pka from the dictionary
-
-
-
 ####
 
 import math
